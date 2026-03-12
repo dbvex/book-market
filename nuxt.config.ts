4cdb1db825dcc880c88aa44466c95b450858699c
Dolet B. <doletbizhev@gmail.com>
2026-03-12 18:17:13 +0300

    chore: fix nuxt config, migrate CI to pnpm, remove dead code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false, // Отключить серверный рендеринг
-  target: 'static',
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   app: {
     baseURL: '/book-market/',
-    middleware: {
-      'manifest-route-rule': { override: true }
-    }
   },
   modules: ['@pinia/nuxt'],
+  css: ['~/assets/styles/global.scss'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           api: 'modern',
           additionalData: `
-            @use "~/assets/styles/main.scss" as *;
             @use "~/assets/styles/_variables.scss" as *;
+            @use "~/assets/styles/_mixins.scss" as *;
           `,
         }
       }
     }
   },
-  // postcss: {
-  //   plugins: {
-  //     'postcss-nested': {},
-  //     'postcss-custom-media': {}
-  //   }
-  // },
 })
